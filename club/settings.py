be397e59efa298edcdda069bdb2760dc1d730c8d
Alexander Tishin <phenyard@gmail.com>
2026-03-22 20:49:05 +0300

    update lock

--- club/settings.py
+++ club/settings.py
@@ -152,7 +152,7 @@ USE_TZ = False
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "frontend/static")]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Task queue (django-q)
 
