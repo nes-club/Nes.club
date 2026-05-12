427f78acb15fb8c4f898efbdf4f668d88767db6f
Alexander Tishin <phenyard@gmail.com>
2026-05-12 18:08:03 +0300

    fix: add AlterModelOptions to badge migration to sync ordering change

--- badges/migrations/0006_remove_badge_price_days.py
+++ badges/migrations/0006_remove_badge_price_days.py
@@ -6,6 +6,10 @@ class Migration(migrations.Migration):
         ("badges", "0005_alter_userbadge_options"),
     ]
     operations = [
+        migrations.AlterModelOptions(
+            name="badge",
+            options={"ordering": ["code"]},
+        ),
         migrations.RemoveField(
             model_name="badge",
             name="price_days",
